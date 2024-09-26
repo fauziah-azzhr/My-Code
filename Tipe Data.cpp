f196ff99d293cfcf4f8751dbaa2d4f99530dcb57
Fauziah Az Zahra <fauziahazzahra@student.unp.ac.id>
2024-09-26 10:27:46 +0700

    Add files via upload

---
+++ Tipe Data.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int x=123456789;
+	float y=36.23;
+	char z='J';
+	
+	printf("Bilangan bulat: %i\n",x);
+	printf("Bilangan pecahan: %.2f\n",y);
+	printf("Inisial: %c\n",z);
+}
